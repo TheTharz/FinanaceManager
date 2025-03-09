53e60bca6c77649fb49e92ea0ccbb91bf0e4e7a1
Tharindu Jayawardhana <tharinduimalka915@gmail.com>
2025-03-09 17:02:23 +0530

    chore: update user service dependencies and add PostgreSQL configuration

---
+++ user_service/src/config/postgresDatabaseConfig.ts
@@ -0,0 +1,28 @@
+import { Pool } from 'pg';
+import dotenv from 'dotenv';
+
+// Load environment variables from a .env file
+dotenv.config();
+
+// Define PostgreSQL connection parameters
+const pool = new Pool({
+  user: process.env.USER_DB_USER,
+  host: 'postgres', // Use the container name or the host IP
+  database: process.env.USER_DB_NAME,
+  password: process.env.USER_DB_PASS,
+  port: 5432,
+});
+
+// Function to test the database connection
+export const testDbConnection = async () => {
+  try {
+    const client = await pool.connect();
+    const res = await client.query('SELECT NOW()');
+    console.log('Database connected:', res.rows[0]);
+    client.release();
+  } catch (err) {
+    console.error('Error connecting to the database:', err);
+  }
+};
+
+export default pool;
