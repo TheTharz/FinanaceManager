0beba34387bf354f42cc981f7952c6677082a963
Tharindu Jayawardhana <tharinduimalka915@gmail.com>
2025-03-10 08:53:57 +0530

    feat: refactor PostgreSQL configuration to use Sequelize and register models

--- user_service/src/config/postgresDatabaseConfig.ts
+++ user_service/src/config/postgresDatabaseConfig.ts
@@ -1,28 +1,31 @@
+import { Sequelize } from 'sequelize-typescript';
 import dotenv from 'dotenv';
-import { Pool } from 'pg';
+import { User } from '../models/userModel';
+import { UserRole } from '../models/userRoleModel';
 
-// Load environment variables from a .env file
 dotenv.config();
 
-// Define PostgreSQL connection parameters
-const pool = new Pool({
-  user: process.env.USER_DB_USER,
-  host: 'postgres', // Use the container name or the host IP
-  database: process.env.USER_DB_NAME,
-  password: process.env.USER_DB_PASS,
-  port: 5432,
+// Initialize Sequelize
+const sequelize = new Sequelize({
+  database: process.env.USER_DB_NAME as string,
+  username: process.env.USER_DB_USER as string,
+  password: process.env.USER_DB_PASS as string,
+  host: process.env.USER_DB_HOST || 'postgres',
+  dialect: 'postgres',
+  port: Number(process.env.USER_DB_PORT) || 5432,
+  models: [User, UserRole], // Register models here
+  logging: false, // Disable SQL logging in production
 });
 
-// Function to test the database connection
+// Function to test database connection
 export const testDbConnection = async () => {
   try {
-    const client = await pool.connect();
-    const res = await client.query('SELECT NOW()');
-    console.log('Database connected:', res.rows[0]);
-    client.release();
-  } catch (err) {
-    console.error('Error connecting to the database:', err);
+    await sequelize.authenticate();
+    console.log('✅ Database connected successfully.');
+  } catch (error) {
+    console.error('❌ Unable to connect to the database:', error);
+    process.exit(1);
   }
 };
 
-export default pool;
+export default sequelize;
